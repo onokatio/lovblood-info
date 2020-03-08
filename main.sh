6baa737396426dc00b6b304302ea380044ab0e2a
onokatio <onokatio@gmail.com>
2020-03-09 00:12:22 +0900

    remove quote

--- main.sh
+++ main.sh
@@ -4,8 +4,6 @@ cat export.html \
 	| pup '.frame-main article section table json{}' > export.json
 
 # 血圧（最高）
-jq '.[0].children[2].children[2].children[2].text' ./export.json
-jq '.[0].children[2].children[2].children[3].text' ./export.json
-jq '.[0].children[2].children[2].children[4].text' ./export.json
-
-rm export.json
+jq -r '.[0].children[2].children[2].children[2].text' ./export.json
+jq -r '.[0].children[2].children[2].children[3].text' ./export.json
+jq -r '.[0].children[2].children[2].children[4].text' ./export.json
